b5d5e78820f1407c50faffb16e1933ffc27b7927
Victor Denisov <denisovenator@gmail.com>
2012-05-21 00:25:10 +0400

    bracket position has been fixed.

--- Tests/Tests.hs
+++ Tests/Tests.hs
@@ -26,7 +26,8 @@ tests = TestList
                  , testCountOccurences "hello" [1, 0, 0, 1, 0, 0, 0, 2, 0, 0, 1]
                  , testCountOccurences (([1, 1, 1])::[Int]) [3]
                  , testCountOccurences (([9, 10, 12])::[Int]) [1, 1, 0, 1]
-                 , testCountOccurences "aabc" [2, 1, 1]]
+                 , testCountOccurences "aabc" [2, 1, 1]
+                 ]
             , TestLabel "partialSums tests" $ TestList
                  [ testPartialSums [1, 1, 1] [1, 2, 3]
                  , testPartialSums [1, 0, 5, 2] [1, 1, 6, 8]
