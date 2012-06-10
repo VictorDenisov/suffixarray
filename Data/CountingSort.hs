bd0ba6d00baab0a46fa3964be7acce99d7e1a627
Victor Denisov <denisovenator@gmail.com>
2012-06-10 14:18:46 +0400

    iforeachr has been rewritten for vectors.

--- Data/CountingSort.hs
+++ Data/CountingSort.hs
@@ -29,17 +29,20 @@ countingSortIO s indexes = do
     let p = partialSums occurences
     pp <- V.unsafeThaw p
     ans <- MVector.replicate n 0
-    iforeachr s $ \i x -> do
+    iforeachr (V.fromList s) $ \i x -> do
         pos <- pp `MVector.read` (index rng x)
         MVector.write ans (pos - 1) (indexes V.! i)
         MVector.write pp (index rng x) (pos - 1)
     V.unsafeFreeze ans
 
 iforeachr :: (Ix a, Ord a, Bounded a)
-          => [a] -> (Int -> a -> IO ()) -> IO ()
-iforeachr s f = zipWithM_ f (reverse [0..(n - 1)]) (reverse s)
+           => V.Vector a -> (Int -> a -> IO ()) -> IO ()
+iforeachr s f = forr ((V.length s) - 1) f
     where
-        n = length s
+        forr 0 f = f 0 (s V.! 0)
+        forr i f = do
+                    f i (s V.! i)
+                    forr (i - 1) f
 
 {- partial sums implementation -}
 
